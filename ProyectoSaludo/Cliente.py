101d62b5f7ad0103fe7ea6d38466dcaa9aaaa115
yober27 <117592995+yober27@users.noreply.github.com>
2025-03-14 19:36:25 -0500

    1.2.1
    ahora si se guardo

---
+++ ProyectoSaludo/Cliente.py
@@ -0,0 +1,28 @@
+######## zona de clases#########
+
+#use crea clase
+class cliente:
+    #use cre ael metodo contructo de la clase
+    def int_(self):
+# se crean los atributos de la clase
+        self.nombre_cliente = ""
+        self.apellido_cliente = ""
+
+# crear metodos get y set por atributos
+    def get_nombre(self):
+        return self.nombre_clinete
+    
+    def get_apellido(self):
+        return self.apellido_cliente
+    
+    def set_nombre(self, dato_nombre):
+        self.nombre_cliente = dato_nombre
+
+    def set_apellido(self, dato_apellido):
+        self.apellido_cliente = dato_apellido
+# se crean metodos normales de la clase
+
+    def tomar_datos(self):
+        pass
+
+    def 
